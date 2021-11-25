4a515bc6271924a0580b43cf31d36401e217fd17
Smeglosiv <jamsahar@kth.se>
2021-11-25 12:13:18 +0100

    Changed back frontend

--- frontend/src/Components/login.component.js
+++ frontend/src/Components/login.component.js
@@ -1,39 +1,74 @@
 import React, {Component} from "react";
-import {Form} from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import axios from "axios";
+import {LOGIN} from "../Constants/Constants";
 import {Route, Switch} from "react-router-dom";
 import SignUp from "./signup.component";
-
+import {Form} from "react-bootstrap";
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {username:'', password:''}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        console.log(event.target.name)
+        this.setState(({[event.target.name]: event.target.value}))
+    }
+    handleSubmit(event){
+        let axiosBody = {userName: this.state.username,password: this.state.password}
+        console.log(axiosBody)
+        let axiosConfig = {headers:{'Content-Type':'application/json'}}
+        let a = axios.post(LOGIN,axiosBody,
+            axiosConfig).then(
+            result => {
+                console.log("Result: " + result)
+                console.log("Result data: " + result.data)
+                console.log("Result data of data: " + result.data.username)
+                localStorage.setItem('user',JSON.stringify(result.data));
+            }
+        )
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <Form>
+                    <form onSubmit={this.handleSubmit}>
                         <h3>Sign In</h3>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email"/>
-                            <Form.Text className="text-muted">
-                                We'll never share your email with anyone else.
-                            </Form.Text>
-                        </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password"/>
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Remember me"/>
-                        </Form.Group>
-                        <Button variant="dark" type="submit">
-                            Submit
-                        </Button>
-                    </Form>
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input type="username" name={"username"} value={this.state.username} onChange={this.handleChange} className="form-control" placeholder="Enter username"/>
+                            <form className="text-muted">
+                                We'll never share your email with anyone else.
+                            </form>
+                        </div>
+
+                        <div className="form-group margin">
+                            <label>Password</label>
+                            <input type="password" name={"password"} value={this.state.password} onChange={this.handleChange} className="form-control" placeholder="Enter password"/>
+                        </div>
+
+                        <div className="form-group">
+                            <div className="custom-control custom-checkbox">
+                                <input type="checkbox" className="custom-control-input" id="customCheck1"/>
+                                <label className="custom-control-label" htmlFor="customCheck1">&nbsp; Remember me</label>
+                            </div>
+                        </div>
+
+                        <div>
+
+                            <button type="submit" className="btn btn-dark btn-block margin">Submit</button>
+
+                        </div>
+                    </form>
                 </div>
             </div>
-
         );
     }
 }
--- frontend/src/Components/signup.component.js
+++ frontend/src/Components/signup.component.js
@@ -1,40 +1,61 @@
 import React, { Component } from "react";
-import {Form} from "react-bootstrap";
+import axios from "axios";
+import {CONFIG,SIGNUP} from "../Constants/Constants";
 import {Route, Switch} from "react-router-dom";
 import Login from "./login.component";
 
+
 export default class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {name:'', username:'', password:''}
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event){
+        console.log(event.target.name)
+        this.setState(({[event.target.name]: event.target.value}))
+    }
+    handleSubmit(event){
+        let axiosBody = {name: this.state.name,userName: this.state.username,password: this.state.password}
+        console.log(axiosBody)
+        let axiosConfig = {headers:{'Content-Type':'application/json'}}
+        let a = axios.post(SIGNUP,axiosBody,
+            axiosConfig).then(
+            result => console.log(result)
+        )
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <Form>
+                    <form onSubmit={this.handleSubmit}>
                         <h3>Sign Up</h3>
-                        <Form.Group className="mb-3" id="formGridFirst">
-                            <label>First name</label>
-                            <input type="text" className="form-control" placeholder="First name" />
-                        </Form.Group>
 
-                        <Form.Group className="mb-3" id="formGridLast">
-                            <label>Last name</label>
-                            <input type="text" className="form-control" placeholder="Last name" />
-                        </Form.Group>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="text" name={"name"} value={this.state.name} onChange={this.handleChange} className="form-control" placeholder="Full name" />
+                        </div>
 
-                        <Form.Group className="mb-3" id="formGridEmail">
-                            <label>Email address</label>
-                            <input type="email" className="form-control" placeholder="Enter email" />
-                        </Form.Group>
+                        <div className="form-group margin">
+                            <label>Username</label>
+                            <input type="username" name={"username"} value={this.state.username} onChange={this.handleChange} className="form-control" placeholder="Enter username" />
+                        </div>
 
-                        <Form.Group className="mb-3" id="formGridPassword">
+                        <div className="form-group margin">
                             <label>Password</label>
-                            <input type="password" className="form-control" placeholder="Enter password" />
-                        </Form.Group>
+                            <input type="password" name={"password"} value={this.state.password} onChange={this.handleChange} className="form-control" placeholder="Enter password" />
+                        </div>
 
-                        <button type="submit" className="btn btn-dark btn-block">Sign Up</button>
+                        <button type="submit" className="btn btn-dark btn-block margin">Sign Up</button>
                         <p className="forgot-password text-right">
                             Already registered <a href="/">sign in?</a>
                         </p>
-                    </Form>
+                    </form>
                 </div>
             </div>
 
